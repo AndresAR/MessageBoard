d630fc63a859f3d1ec3b0682088555655c6dfcf2
Andres Eduardo <aariasr@MBP-de-Andres.lan>
2015-09-26 20:59:17 -0300

    few changes and fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,12 +7,17 @@ class MessagesController < ApplicationController
 	end
 
 	def create
-		@messsage = Messsag.new(message_params)
+		@message = Message.new(message_params)
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
 	end
 
 	private
 
 		def message_params
-			params.requiere(:messsage).permit(:title, :description) 
+			params.require(:message).permit(:title, :description) 
 		end
 end
